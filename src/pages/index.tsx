83a85667aaa6a211e7dd09804c5f80a7e5452c39
gamadv <lealdev.py@gmail.com>
2023-05-05 18:02:19 -0300

    feat: Create carousel with KeenSlider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,80 @@
-import { HomeContainer, Product } from "@/styles/pages/home";
 import Image from "next/image";
+import { useKeenSlider } from "keen-slider/react";
+
+import { HomeContainer, Product } from "@/styles/pages/home";
+
+import "keen-slider/keen-slider.min.css";
 
 import cam1 from "../assets/cam1.png";
 import cam2 from "../assets/cam2.png";
 import cam3 from "../assets/cam3.png";
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 4,
+      spacing: 48,
+    },
+  });
   return (
-    <>
-      <HomeContainer>
-        <Product>
-          <Image src={cam1} width={520} height={480} alt="" />
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
+        <Image src={cam1} width={520} height={480} alt="" />
 
-          <footer>
-            <strong>Camiseta X</strong>
-            <span>R$ 79,90</span>
-          </footer>
-        </Product>
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
 
-        <Product>
-          <Image src={cam2} width={520} height={480} alt="" />
+      <Product className="keen-slider__slide">
+        <Image src={cam2} width={520} height={480} alt="" />
 
-          <footer>
-            <strong>Camiseta X</strong>
-            <span>R$ 79,90</span>
-          </footer>
-        </Product>
-      </HomeContainer>
-    </>
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={cam3} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta 3</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={cam3} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta 3</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={cam3} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta 3</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={cam2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta 3</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={cam2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta 3</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   );
 }
