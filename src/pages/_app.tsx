af7ff3cc30275127e3047d553a5d0c3a6d35bca7
gamadv <lealdev.py@gmail.com>
2023-05-05 17:51:18 -0300

    feat: Add mock imgs and create home structure

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,15 @@ import type { AppProps } from "next/app";
 import logoImg from "../assets/logo.svg";
 import { Container, Header } from "../styles/pages/app";
 
+import Image from "next/image";
+
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={logoImg.src} alt="" />
+        <Image src={logoImg.src} alt="" width={150} height={150} />
       </Header>
 
       <Component {...pageProps} />
